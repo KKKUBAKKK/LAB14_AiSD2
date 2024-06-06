66d61ab16c23a1430737f6ae5bc5ff21fbc8bffc
KKKUBAKKK <kuba.kindracki@gmail.com>
2024-06-06 10:17:45 +0200

    Initial commit

---
+++ LAB14_AiSD2/Lab14.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labratoria_ASD2_2024
+{
+    public class Lab14 : MarshalByRefObject
+    {
+        /// <summary>
+        /// Znajduje wszystkie maksymalne palindromy długości przynajmniej 2 w zadanym słowie. Wykorzystuje Algorytm Manachera.
+        /// 
+        /// Palindromy powinny być zwracane jako lista par (indeks pierwszego znaku, długość palindromu), 
+        /// tzn. para (i, d) oznacza, że pod indeksem i znajduje się pierwszy znak d-znakowego palindromu.
+        /// 
+        /// Kolejność wyników nie ma znaczenia.
+        /// 
+        /// Można założyć, że w tekście wejściowym nie występują znaki '#' i '$' - można je wykorzystać w roli wartowników
+        /// </summary>
+        /// <param name="text">Tekst wejściowy</param>
+        /// <returns>Tablica znalezionych palindromów</returns>
+        public (int startIndex, int length)[] FindPalindromes(string text)
+        {
+            return null;
+
+        }
+    }
+
+}
